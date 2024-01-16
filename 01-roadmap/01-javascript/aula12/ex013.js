c3443bbfdf5768cc5c585444685abc890acaedf4
Carlos Chiarelli <cchiarelli@auctus.ai>
2024-01-16 16:38:49 +0000

    update if else switch case

---
+++ 01-roadmap/01-javascript/aula12/ex013.js
@@ -0,0 +1,39 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+0 = dom
+1 = seg
+2 = terc
+3 = qua
+4 = qui
+5 = sex
+6 = sab
+*/
+console.log(diaSem)
+
+switch (diaSem) {
+  case 0:
+    console.log("domingo");
+    break;
+  case 1:
+    console.log("segunda");
+    break;
+  case 2:
+    console.log("terça");
+    break;
+  case 3:
+    console.log("quarta");
+    break;
+  case 4:
+    console.log("quinta");
+    break;
+  case 5:
+    console.log("sexta");
+    break;
+  case 6:
+    console.log("sábado");
+    break;
+  default:
+    console.log('[ERRO] Dia inválido!')
+    break;
+}
